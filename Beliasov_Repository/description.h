a8a567314699641feefeaa618c98892f2a407e96
Gleb Beliasov <gleeebsss@gmail.com>
2023-12-28 17:18:49 +0300

    gb

---
+++ Beliasov_Repository/description.h
@@ -0,0 +1,8 @@
+﻿/**
+	@mainpage Проект LR3 для создания комментарий в стиле Doxygen для файлов проекта LR1
+	@copyright ВоГУ
+	@author  Белясов Г.Ю.   
+	@date 28 - 12 - 2023
+\par Содержит файлы:
+- @ref Beliasov_Repository.cpp
+*/
