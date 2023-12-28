28a26781776f0a574af10058029b905c0301666e
Gleb Beliasov <gleeebsss@gmail.com>
2023-12-28 18:11:56 +0300

    final

---
+++ packet.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft Visual Studio \2019\Common7\IDE\
+vcn.exe" Beliasov_Repository.sln /build Debug
+"E:\doxygen\bin\doxygen.exe" myConfig
+"E:\Inno Setup 6\ISCC.exe" build.iss
