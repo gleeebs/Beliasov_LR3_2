50d5f664629ed2754e9d52b768dc94c069b71d0c
Gleb Beliasov <gleeebsss@gmail.com>
2023-12-29 01:38:56 +0300

    update

--- packet.bat
+++ packet.bat
@@ -1,4 +1,6 @@
 "C:\Program Files (x86)\Microsoft Visual Studio \2019\Common7\IDE\
 vcn.exe" Beliasov_Repository.sln /build Debug
 "E:\doxygen\bin\doxygen.exe" myConfig
-"E:\Inno Setup 6\ISCC.exe" build.iss
+"E:\Inno Setup 6\ISCC.exe" build1.iss
+"E:\Inno Setup 6\ISCC.exe" build2.iss
+"E:\Inno Setup 6\ISCC.exe" build3.iss
